7c7c366810bda93210e46c30cf439265b6149f7b
Peri Carney <pericarney@gmail.com>
2025-07-07 17:09:37 -0500

    I created querySelectors for each button and assigned each one to show the text on its corresponding button. Next steps is creating the event to occur after you select an answer.

--- app.js
+++ app.js
@@ -17,18 +17,29 @@ const quiz1Key = []
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
 /*------------------------ Cached Element References ------------------------*/
 
 const questionEl = document.querySelector("#question")
 let choiceBtnEl = document.querySelectorAll(".choice")
 
+questionEl.textContent = quiz1Questions[0].question
+
+const choiceBtn1El = document.querySelector("#b1")
+choiceBtn1El.textContent = quiz1Questions[0].possibleAnswers[0];
+
+const choiceBtn2El = document.querySelector("#b2");
+choiceBtn2El.textContent = quiz1Questions[0].possibleAnswers[1];
+
+const choiceBtn3El = document.querySelector("#b3");
+choiceBtn3El.textContent = quiz1Questions[0].possibleAnswers[2];
+
+const choiceBtn4El = document.querySelector("#b4");
+choiceBtn4El.textContent = quiz1Questions[0].possibleAnswers[3];
 /*----------------------------- Event Listeners -----------------------------*/
 
 choiceBtnEl.forEach((choice, id) => {
     choice.addEventListener("click", () => {console.log("You clicked me")})
 })
-
 
 /*----------------------------- Comment Graveyard -----------------------------*/
 
