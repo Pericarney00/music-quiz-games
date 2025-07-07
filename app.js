8a1b2bf09eeff5a1047500864e0c240b589587cd
Peri Carney <pericarney@gmail.com>
2025-07-07 15:21:48 -0500

    Created a h1 & h2 and buttons for the HTML. I created an object in JS for the first question

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
- 
+/*-------------------------------- Constants --------------------------------*/
+const quiz1Questions = [
+    {
+        question:"What is the name of this group?",
+        possibleAnswers: [
+            "SF9",
+            "Stray Kids",
+            "Ateez",
+            "BTS"
+        ],
+        correctAnswer: "Stray Kids"
+    },
+    
+]
+
+const quiz1Key = []
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+/*----------------------------- Comment Graveyard -----------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
