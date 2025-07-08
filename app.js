c09d16d866190b12796099c73605191a2335bd5a
Peri Carney <pericarney@gmail.com>
2025-07-08 09:54:09 -0500

    Created a checkScore function and added elements to HTML to display the image for the question and if they were correct or incorrect

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 /*-------------------------------- Constants --------------------------------*/
 const quiz1Questions = [
-    {
-        question:"What is the name of this group?",
-        possibleAnswers: [
-            "SF9",
-            "Stray Kids",
-            "Ateez",
-            "BTS"
-        ],
-        correctAnswer: "Stray Kids"
-    },
-
-]
+  {
+    question: "What is the name of this group?",
+    possibleAnswers: ["SF9", "Stray Kids", "Ateez", "BTS"],
+    correctAnswer: "Stray Kids",
+  },
+  {
+    question: "What is the name of this group?",
+    possibleAnswers: ["BTOB", "IKON", "NCT", "SHINee"],
+    correctAnswer: "SHINee",
+  },
+];
 
 const quiz1Key = []
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let total = 0
 /*------------------------ Cached Element References ------------------------*/
 
 const questionEl = document.querySelector("#question")
@@ -35,16 +34,27 @@ choiceBtn3El.textContent = quiz1Questions[0].possibleAnswers[2];
 
 const choiceBtn4El = document.querySelector("#b4");
 choiceBtn4El.textContent = quiz1Questions[0].possibleAnswers[3];
-/*----------------------------- Event Listeners -----------------------------*/
 
-choiceBtnEl.forEach((choice, id) => {
-    choice.addEventListener("click", () => {console.log("You clicked me")})
-})
+
+//if possible answer === correct answer then total = total + 1
 
 /*----------------------------- Comment Graveyard -----------------------------*/
 
 
 /*-------------------------------- Functions --------------------------------*/
 
+const checkScore = (event) => {
+    console.log(quiz1Questions[0].correctAnswer)
+    if (event.target.innerText === quiz1Questions[0].correctAnswer) {
+        total = total + 1;
+        console.log("Correct")
+    } else {
+        
+    }
+} 
 
+/*----------------------------- Event Listeners -----------------------------*/
 
+choiceBtnEl.forEach((choice, id) => {
+    choice.addEventListener("click", checkScore)
+})
