bdab7631226bd51e0d82e93ee8df81f15c26a11a
Peri Carney <pericarney@gmail.com>
2025-07-08 16:52:11 -0500

    filled in the rest of the data for the quiz1 array. I updated the README Attributions with the images used in the quiz.

--- README.md
+++ README.md
@@ -16,8 +16,16 @@ Links:
 Images
 -[Stray Kids Image]("https://thebiaslist.com/wp-content/uploads/2025/06/stray-kids-hollow.jpg")
 -[SHINee Image](https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgE4nqeaO3VhdOVG0AuxHfRlS4ZS6GNsERz9xzy3USDqO_EJaJLGHAapp1PyZ9r6HhIHYUe_C5tx6N7ZH4jzEQ48pnfXykkDSTXBBNnUDCBIF840DdE8DUll05Xj4274o3YwbQ-mhdElMoH/s640/shinee.jpg)
--
--
+-[Seventeen Image]("https://dailynexus.s3.us-west-1.amazonaws.com/dailynexus/wp-content/uploads/2022/09/22150717/kpop-draft-1.jpg",
+)
+-[BTOB Image](https://kpopreviewed.com/wp-content/uploads/2017/03/btobfeelem.jpg)
+-[Ateez](https://images.squarespace-cdn.com/content/v1/56eb012f27d4bd29de975fae/1632602667384-6BGZ7CZ4CSHGH4KWBRX2/HIST_ATEEZ_Header.jpg?format=1500w)
+-[SF9](https://i.scdn.co/image/ab67616d0000b273af097df559ed0fa3819705d5)
+-[BTS](https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/bts.png)
+-[IKON](https://i0.wp.com/ulzza.com/wp-content/uploads/2019/07/iKONs-First-Public-Appearance-As-A-6-Member-Group.jpg?w=700&ssl=1)
+-[NCT](https://pm1.aminoapps.com/7048/67445bd683e0f134c2e5f3e48b947f86f5cc94e3r1-720-400v2_hq.jpg)
+-[TXT](https://c.ndtvimg.com/2024-11/iq7ctal8_txt_625x300_18_November_24.jpg?downsize=773:435)
+
 ## **Technologies used**: List of the technologies used, for example: JavaScript, HTML, CSS, etc.
 Next steps: Planned future enhancements (stretch goals).
 
--- app.js
+++ app.js
@@ -1,17 +1,74 @@
 /*-------------------------------- Constants --------------------------------*/
 const quiz1Questions = [
-    {
+  {
     question: "What is the name of this group?",
-    imgUrl: "https://thebiaslist.com/wp-content/uploads/2025/06/stray-kids-hollow.jpg",
+    imgUrl:
+      "https://thebiaslist.com/wp-content/uploads/2025/06/stray-kids-hollow.jpg",
     possibleAnswers: ["SF9", "Stray Kids", "Ateez", "BTS"],
     correctAnswer: "Stray Kids",
-    },
-    {
+  },
+  {
     question: "What is the name of this group?",
-    imgUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgE4nqeaO3VhdOVG0AuxHfRlS4ZS6GNsERz9xzy3USDqO_EJaJLGHAapp1PyZ9r6HhIHYUe_C5tx6N7ZH4jzEQ48pnfXykkDSTXBBNnUDCBIF840DdE8DUll05Xj4274o3YwbQ-mhdElMoH/s640/shinee.jpg",
+    imgUrl:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgE4nqeaO3VhdOVG0AuxHfRlS4ZS6GNsERz9xzy3USDqO_EJaJLGHAapp1PyZ9r6HhIHYUe_C5tx6N7ZH4jzEQ48pnfXykkDSTXBBNnUDCBIF840DdE8DUll05Xj4274o3YwbQ-mhdElMoH/s640/shinee.jpg",
     possibleAnswers: ["BTOB", "IKON", "NCT", "SHINee"],
     correctAnswer: "SHINee",
-    },
+  },
+  {
+    question: "What is the name of this group?",
+    imgUrl:
+      "https://dailynexus.s3.us-west-1.amazonaws.com/dailynexus/wp-content/uploads/2022/09/22150717/kpop-draft-1.jpg",
+    possibleAnswers: ["TXT", "Stray Kids", "Seventeen", "Ateez"],
+    correctAnswer: "Seventeen",
+  },
+  {
+    question: "What is the name of this group?",
+    imgUrl:
+      "https://kpopreviewed.com/wp-content/uploads/2017/03/btobfeelem.jpg",
+    possibleAnswers: ["BTOB", "SHINee", "SF9", "MonstaX"],
+    correctAnswer: "BTOB",
+  },
+  {
+    question: "What is the name of this group?",
+    imgUrl:
+      "https://images.squarespace-cdn.com/content/v1/56eb012f27d4bd29de975fae/1632602667384-6BGZ7CZ4CSHGH4KWBRX2/HIST_ATEEZ_Header.jpg?format=1500w",
+    possibleAnswers: ["Ateez", "IKON", "SF9", "MonstaX"],
+    correctAnswer: "Ateez",
+  },
+  {
+    question: "What is the name of this group?",
+    imgUrl: "https://i.scdn.co/image/ab67616d0000b273af097df559ed0fa3819705d5",
+    possibleAnswers: ["BTS", "MonstaX", "SF9", "Stray Kids"],
+    correctAnswer: "SF9",
+  },
+  {
+    question: "What is the name of this group?",
+    imgUrl:
+      "https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/bts.png",
+    possibleAnswers: ["Ateez", "SHINee", "EXO", "BTS"],
+    correctAnswer: "BTS",
+  },
+  {
+    question: "What is the name of this group?",
+    imgUrl:
+      "https://i0.wp.com/ulzza.com/wp-content/uploads/2019/07/iKONs-First-Public-Appearance-As-A-6-Member-Group.jpg?w=700&ssl=1",
+    possibleAnswers: ["SF9", "IKON", "Seventeen", "MonsterX"],
+    correctAnswer: "IKON",
+  },
+  {
+    question: "What is the name of this group?",
+    imgUrl:
+      "https://pm1.aminoapps.com/7048/67445bd683e0f134c2e5f3e48b947f86f5cc94e3r1-720-400v2_hq.jpg",
+    possibleAnswers: ["Enhypen", "SHINee", "SF9", "NCT"],
+    correctAnswer: "NCT",
+  },
+  {
+    question: "What is the name of this group?",
+    imgUrl:
+      "https://c.ndtvimg.com/2024-11/iq7ctal8_txt_625x300_18_November_24.jpg?downsize=773:435",
+    possibleAnswers: ["BTOB", "SHINee", "TXT", "The Boyz"],
+    correctAnswer: "TXT",
+  },
 ];
 
 const quiz1Key = []
